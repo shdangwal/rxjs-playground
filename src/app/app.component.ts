365107eb16659b808d686f612ce5f3632333abe6
Shubham Dangwal <shdangwal@gmail.com>
2025-01-02 20:14:10 +0530

    batman

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'rxjs-playground';
+  data: number = 0;
+  constructor() {
+    const numbers$ = of([1, 2, 3, 4, 5]);
+
+    numbers$.subscribe((data) => {
+      console.log('subscriber using of', data);
+    });
+
+    const numbers1$ = from([1, 2, 3, 4, 5]);
+
+    numbers1$.subscribe((data) => {
+      console.log('subscriber using from', data);
+      this.data = data;
+    });
+  }
 }
